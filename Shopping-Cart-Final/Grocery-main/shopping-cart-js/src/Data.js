fad84c8119e61689217e46183c5a47ead9a17b98
HEMANTH U SHETTY <126490029+ShettyHemanth@users.noreply.github.com>
2023-04-09 11:08:55 +0530

    Add files via upload

---
+++ Shopping-Cart-Final/Grocery-main/shopping-cart-js/src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "ORANGES",
+    price: 50,
+    desc: "Rich source of Vitamin C, Contains Vitamin K, Amino acid.Orange helps improving the skin health.",
+    img: "images/product-1.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "ONIONS",
+    price: 60,
+    desc: "Contains Folic acid, Vitamin C and Amino acid .Onion is a staple in India and are commonly chopped and used as an ingredient in various hearty warm dishes. ",
+    img: "images/product-2.png",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "MEAT",
+    price: 250,
+    desc: "chicken is low in calories but high in protein, it may be especially beneficial for weight loss, if that’s a goal for you.",
+    img: "images/product-3.png",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "CABBAGE",
+    price: 40,
+    desc: "Cabbage is an exceptionally healthy food. It has an outstanding nutrient profile and is especially high in vitamins C and K. ",
+    img: "images/product-4.png",
+  },
+  {
+    id: "thiecbawdjksadjk",
+    name: "POTATOE",
+    price: 35,
+    desc: "New Potatoes: If you are looking for soft, slightly sweet but creamy-textured potatoes. These are a special variant early harvest of potatoes",
+    img: "images/product-5.png",
+  },
+  {
+    id: "iuertrywebncdjksadjk",
+    name: "AVACARDO",
+    price: 200,
+    desc: "Green skinned and fleshy, Avocado (Butter Fruit) has a smooth texture and is widely used to make Mexican dips such as Guacamole. This exotic food is often used in the preparation of sweet and savoury dishes and can be eaten raw in salads or served with spices.",
+    img: "images/product-6.png",
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "CARROT",
+    price: 70,
+    desc: "Sweet and crunchy, Ooty Carrots contain notably high levels of the cartenoid lutein, anti-oxidants and dietary fibres. These are beneficial for healthy skin and eyes.",
+    img: "images/product-7.png",
+  },
+  {
+    id: "trfoiwfcnbcawdjksadjk",
+    name: "LEMON",
+    price: 20,
+    desc: "Lemon is a popular fruit that is used in small quantities to add flavor to food and brings joy to the palate and bliss to life. Lemons give flavor to baked goods, sauces, salad dressings, marinades, drinks, and desserts.",
+    img: "images/product-8.png",
+  }
+];
